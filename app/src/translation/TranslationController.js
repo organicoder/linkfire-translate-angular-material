2e81878e52d5c86e66798c800e9ce2e5ae767b02
Thomas Saaby Noer <tssaby@gmail.com>
2016-02-03 14:39:31 +0100

    Translation component added - needs work on the data in translationService

---
+++ app/src/translation/TranslationController.js
@@ -0,0 +1,90 @@
+(function(){
+
+  angular
+       .module('translation')
+       .controller('TranslationController', [
+          'translationService', '$mdSidenav', '$mdBottomSheet', '$log', '$q',
+          TranslationController
+       ]);
+
+  /**
+   * Main Controller for the Angular Material Starter App
+   * @param $scope
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+  function TranslationController( translationService, $mdSidenav, $mdBottomSheet, $log) {
+    var self = this;
+
+    self.selected     = null;
+    self.translations        = [ ];
+    self.selectTranslation   = selectTranslation;
+    self.toggleList   = toggleUsersList;
+    self.makeContact  = makeContact;
+
+    // Load all translations
+
+    translationService
+          .loadAllTranslations()
+          .then( function( translation ) {
+            self.translation    = [].concat(translation);
+            self.selected = translation[0];
+          });
+
+    // *********************************
+    // Internal methods
+    // *********************************
+
+    /**
+     * Hide or Show the 'left' sideNav area
+     */
+    function toggleUsersList() {
+      $mdSidenav('left').toggle();
+    }
+
+    /**
+     * Select the current avatars
+     * @param menuId
+     */
+    function selectTranslation ( user ) {
+      self.selected = angular.isNumber(user) ? $scope.users[user] : user;
+    }
+
+    /**
+     * Show the Contact view in the bottom sheet
+     */
+    function makeContact(selectedTranslation) {
+
+        $mdBottomSheet.show({
+          controllerAs  : "cp",
+          templateUrl   : './src/users/view/contactSheet.html',
+          controller    : [ '$mdBottomSheet', ContactSheetController],
+          parent        : angular.element(document.getElementById('content'))
+        }).then(function(clickedItem) {
+          $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        /**
+         * User ContactSheet controller
+         */
+        function ContactSheetController( $mdBottomSheet ) {
+          this.user = selectedUser;
+          this.actions = [
+            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
+            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
+            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
+            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
+          ];
+          this.contactUser = function(action) {
+            // The actually contact process has not been implemented...
+            // so just hide the bottomSheet
+
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
+
+  }
+
+})();
---
+++ app/src/translation/TranslationService.js
@@ -0,0 +1,94 @@
+(function(){
+  'use strict';
+
+  angular.module('translations')
+         .service('translationService', ['$q', TranslationService]);
+
+  /**
+   * Translations DataService
+   * Uses embedded, hard-coded data model; acts asynchronously to simulate
+   * remote data service call(s).
+   *
+   * @returns {{loadAll: Function}}
+   * @constructor
+   */
+  function TranslationService($q){
+    var translations = [
+      {
+        name: 'Default (English)',
+        avatar: 'svg-1',
+        content: 'Test Test Test Test Test Test Test Test Test Test Test Test Test'
+      },
+      {
+        name: 'German',
+        avatar: 'svg-2',
+        content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
+      },
+      {
+        name: 'French',
+        avatar: 'svg-3',
+        content: "Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney's blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS."
+      },
+      {
+        name: 'Spanish',
+        avatar: 'svg-4',
+        content: 'Scratch the furniture spit up on light gray carpet instead of adjacent linoleum so eat a plant, kill a hand pelt around the house and up and down stairs chasing phantoms run in circles, or claw drapes. Always hungry pelt around the house and up and down stairs chasing phantoms.'
+      },
+      {
+        name: 'Italian',
+        avatar: 'svg-5',
+        content: 'Webtwo ipsum dolor sit amet, eskobo chumby doostang bebo. Bubbli greplin stypi prezi mzinga heroku wakoopa, shopify airbnb dogster dopplr gooru jumo, reddit plickers edmodo stypi zillow etsy.'
+      },
+      {
+        name: 'Swedish',
+        avatar: 'svg-6',
+        content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
+      },
+      {
+        name: 'Norwegian',
+        avatar: 'svg-6',
+        content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
+      },
+      {
+        name: 'Danish',
+        avatar: 'svg-6',
+        content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
+      },
+      {
+        name: 'Finnish',
+        avatar: 'svg-6',
+        content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
+      }
+    ];
+
+    [
+  {
+    "GENERAL": "General"
+  }, 
+  {
+    "USER_MANAGEMENT": "User management"
+  }, 
+  {
+    "MUSIC_SERVICE_MANAGEMENT": "Music service management"
+  }, 
+  {
+    "CUSTOMIZATION": "Customization"
+  }, 
+  {
+    "CHANNEL_MANAGEMENT": "Channel management"
+  }, 
+  {
+    "AFFILIATE_PROGRAMS": "Affiliate programs"
+  }
+];
+
+    // Promise-based API
+    return {
+      loadAllTranslations : function() {
+        // Simulate async nature of real remote calls
+        return $q.when(translations);
+      }
+    };
+  }
+
+})();
