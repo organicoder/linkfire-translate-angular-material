0b6692e0aff2d5027c138c15c2ed276aebc6f61e
Thomas Saaby Noer <tssaby@gmail.com>
2016-02-10 13:21:33 +0100

    fixed shit

--- app/src/maindata/MaindataService.js
+++ app/src/maindata/MaindataService.js
@@ -61,7 +61,6 @@
         content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
       }
     ];
->>>>>>> master
 
     // Promise-based API
     return {
